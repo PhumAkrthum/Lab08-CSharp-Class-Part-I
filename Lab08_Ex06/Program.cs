e31960433b68c79ea79afebe8cf1fc5eae6154ca
PhumAkrthum <66030270@kmitl.ac.th>
2025-02-01 00:03:38 +0700

    update6

---
+++ Lab08_Ex06/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+var mc = new MyClass();
+mc.DisplayParam(3, 4);
+class MyClass
+{
+    public void DisplayParam(int a, int b)
+    {
+     System.Console.WriteLine($"a = {a}, b = {b}");
+    } 
+}
