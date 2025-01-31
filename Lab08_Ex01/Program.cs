6692aeeb962b64ec590d061ae574e1661da88ceb
PhumAkrthum <66030270@kmitl.ac.th>
2025-01-31 17:15:49 +0700

    update1

---
+++ Lab08_Ex01/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Person p = new Person();
+   System.Console.WriteLine($"Type of p is {p.GetType()}");
+   
+   class Person
+   {
+   
+   }
